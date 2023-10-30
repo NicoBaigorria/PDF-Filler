142e3f49f7877db472f92896cd37b760e2724022
Nicolás Baigorria <56775102+NicoBaigorria@users.noreply.github.com>
2023-10-30 12:08:14 -0300

    Commit 30/10/23

---
+++ PDF-Filler/FormFiller.cs
@@ -0,0 +1,149 @@
+﻿using Syncfusion.Pdf.Xfa;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PDF_Filler
+{
+    public class FormFiller
+    {
+
+        public void Process(string inputFile) {
+
+
+            string GetFieldType(string text)
+            {
+
+                string[] parts = text.Split('.');
+
+                string className = parts[parts.Length - 1];
+
+                return className;
+            }
+ 
+            HashSet<string> listFieldsType = new HashSet<string>();
+
+            //Load the PDF document.
+            using (FileStream docStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            {
+                //Load the existing XFA document.
+                PdfLoadedXfaDocument loadedDocument = new PdfLoadedXfaDocument(docStream);
+                //Load the existing XFA form.
+                PdfLoadedXfaForm loadedForm = loadedDocument.XfaForm;
+                //Get the complete field names
+                string[] completeFieldNames = loadedForm.CompleteFieldNames;
+
+                Console.WriteLine(completeFieldNames.Length);
+
+                var data = new Dictionary<string, object>();
+
+                foreach (string nameField in completeFieldNames)
+                {
+                    //Console.WriteLine(nameField);
+
+                    PdfLoadedXfaField field = loadedForm.TryGetFieldByCompleteName(nameField);
+
+                    if (field != null)
+                    {
+                        /*
+                        try
+                        {
+                           // (field as PdfLoadedXfaTextBoxField).Text = "dfgfdgfdgdf";
+                            // Display the field value.
+                            Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaTextBoxField).Text}");
+                        }
+                        catch (Exception e) {
+
+                            Console.WriteLine($"{field.Name}: {tipo}");
+                        }
+                        */
+
+
+                        string tipo = GetFieldType(field.ToString());
+
+                        listFieldsType.Add(tipo);
+
+                        //Console.WriteLine($"{field.Name}: {tipo}");
+
+                        data[field.Name] = new { tipo = tipo };
+
+
+                        switch (tipo)
+                        {
+                            case "PdfLoadedXfaTextBoxField":
+                                Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaTextBoxField).Text}");
+                                data[field.Name] = new { value = (field as PdfLoadedXfaTextBoxField).Text, type = "texto" };
+                                break;
+                            case "PdfLoadedXfaComboBoxField":
+                                List<string> fields = new List<string>((field as PdfLoadedXfaComboBoxField).Items);
+                                Console.WriteLine($"{field.Name}:");
+                                data[field.Name] = new { values = fields, type = "selector multiple" };
+                                foreach (string name in fields)
+                                {
+                                    Console.WriteLine($"* {name}");
+                                }
+                                break;
+                            case "PdfLoadedXfaRadioButtonGroup":
+                                List<PdfLoadedXfaRadioButtonField> fields2 = new List<PdfLoadedXfaRadioButtonField>((field as PdfLoadedXfaRadioButtonGroup).Fields);
+                                Console.WriteLine($"{field.Name}:");
+
+                                List<string> options = new List<string>();
+                                foreach (PdfLoadedXfaRadioButtonField name in fields2)
+                                {
+                                    options.Add(name.Name.ToString());
+                                    Console.WriteLine($"* {name.Name}");
+                                }
+
+                                data[field.Name] = new { type = "selector", values = fields2,options = options };
+
+                                break;
+                            case "PdfLoadedXfaDateTimeField":
+                                data[field.Name] = new { value = (field as PdfLoadedXfaDateTimeField).Value, type = "fecha" };
+                                Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaDateTimeField).Value}");
+                                break;
+                            case "PdfLoadedXfaCheckBoxField":
+                                data[field.Name] = new { value = (field as PdfLoadedXfaCheckBoxField).IsChecked, type = "booleano" };
+                                Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaCheckBoxField).IsChecked}");
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Field not found.");
+                    }
+                }
+
+                string json = JsonSerializer.Serialize(data);
+
+
+                foreach (string key in listFieldsType)
+                {
+                    Console.WriteLine($"{key}");
+                }
+
+
+                string fileName = Path.GetFileNameWithoutExtension(docStream.Name);
+
+                string jsonFilePath = "C:\\Users\\Usuario\\source\\repos\\PDF-Filler\\PDF-Filler\\PropsFiles\\" + fileName + ".json";
+
+                // Save the JSON to the file
+                File.WriteAllText(jsonFilePath, json);
+
+                string outputFile = "C:\\Users\\Usuario\\source\\repos\\PDF-Filler\\PDF-Filler\\OutputFiles\\"+ fileName +".pdf"; // Replace with the path for the filled PDF.
+
+                //Create memory stream.
+                FileStream docStream2 = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
+                //Save the document to memory stream.
+                loadedDocument.Save(docStream2);
+                //Close the document.
+                loadedDocument.Close();
+            }
+        }
+    }
+}
--- PDF-Filler/Program.cs
+++ PDF-Filler/Program.cs
@@ -13,136 +13,39 @@ using System.Text.Json.Nodes;
 using System.Text.Json;
 using System.Linq.Expressions;
 using Path = System.IO.Path;
+using PDF_Filler;
 
 class Program
 {
 
     static void Main()
     {
+        FormFiller filler = new FormFiller();
 
-        string GetFieldType(string text)
+        string folderPath = @"C:\Users\Usuario\source\repos\PDF-Filler\PDF-Filler\InputFiles\"; // Replace with the path to your folder
+
+        if (Directory.Exists(folderPath))
         {
+            string[] pdfFiles = Directory.GetFiles(folderPath, "*.pdf", SearchOption.TopDirectoryOnly);
 
-            string[] parts = text.Split('.');
-
-            string className = parts[parts.Length - 1];
-
-            return className;
-        }
-
-        string inputFile = "C:\\Users\\Usuario\\source\\repos\\PDF-Filler\\PDF-Filler\\InputFiles\\imm1294e.pdf"; // Replace with the path to your input XFA PDF form.
-        string outputFile = "C:\\Users\\Usuario\\source\\repos\\PDF-Filler\\PDF-Filler\\OutputFiles\\imm1294e.pdf"; // Replace with the path for the filled PDF.
-
-        HashSet<string> listFieldsType = new HashSet<string>();
-
-        //Load the PDF document.
-        using (FileStream docStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
-        {
-            //Load the existing XFA document.
-            PdfLoadedXfaDocument loadedDocument = new PdfLoadedXfaDocument(docStream);
-            //Load the existing XFA form.
-            PdfLoadedXfaForm loadedForm = loadedDocument.XfaForm;
-            //Get the complete field names
-            string[] completeFieldNames = loadedForm.CompleteFieldNames;
-
-            Console.WriteLine(completeFieldNames.Length);
-
-            var data = new Dictionary<string, object>();
-
-            foreach (string nameField in completeFieldNames)
+            if (pdfFiles.Length > 0)
             {
-                //Console.WriteLine(nameField);
+                Console.WriteLine("PDF files found in the folder:");
 
-                PdfLoadedXfaField field = loadedForm.TryGetFieldByCompleteName(nameField);
-
-                if (field != null)
+                foreach (string pdfFile in pdfFiles)
                 {
-                    /*
-                    try
-                    {
-                       // (field as PdfLoadedXfaTextBoxField).Text = "dfgfdgfdgdf";
-                        // Display the field value.
-                        Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaTextBoxField).Text}");
-                    }
-                    catch (Exception e) {
-
-                        Console.WriteLine($"{field.Name}: {tipo}");
-                    }
-                    */
-
-
-                    string tipo = GetFieldType(field.ToString());
-
-                    listFieldsType.Add(tipo);
-
-                    //Console.WriteLine($"{field.Name}: {tipo}");
-
-                    data[field.Name] = new { tipo= tipo };
-
-
-                    switch (tipo)
-                    {
-                        case "PdfLoadedXfaTextBoxField":
-                            Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaTextBoxField).Text}");
-                            data[field.Name] = new { value = (field as PdfLoadedXfaTextBoxField).Text };
-                            break;
-                        case "PdfLoadedXfaComboBoxField":
-                            List<string> fields = new List<string>((field as PdfLoadedXfaComboBoxField).Items);
-                            Console.WriteLine($"{field.Name}:");
-                            data[field.Name] = new {values = fields };
-                            foreach (string name in fields)
-                            {
-                                Console.WriteLine($"* {name}");
-                            }
-                            break;
-                        case "PdfLoadedXfaRadioButtonGroup":
-                            List<PdfLoadedXfaRadioButtonField> fields2 = new List<PdfLoadedXfaRadioButtonField>((field as PdfLoadedXfaRadioButtonGroup).Fields);
-                            Console.WriteLine($"{field.Name}:");
-                            data[field.Name] = new { values = fields2 };
-                            foreach (PdfLoadedXfaRadioButtonField name in fields2)
-                            {
-                                Console.WriteLine($"* {name.Name}");
-                            }
-                            break;
-                        case "PdfLoadedXfaDateTimeField":
-                            data[field.Name] = new { value = (field as PdfLoadedXfaDateTimeField).Value };
-                            Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaDateTimeField).Value}");
-                            break;
-                        case "PdfLoadedXfaCheckBoxField":
-                            data[field.Name] = new { value = (field as PdfLoadedXfaCheckBoxField).IsChecked };
-                            Console.WriteLine($"{field.Name}: {(field as PdfLoadedXfaCheckBoxField).IsChecked}");
-                            break;
-                    }
-
-                }
-                else
-                {
-                    Console.WriteLine("Field not found.");
+                    Console.WriteLine(pdfFile);
+                    filler.Process(pdfFile);
                 }
             }
-
-            string json = JsonSerializer.Serialize(data);
-
-
-            foreach (string key in listFieldsType)
+            else
             {
-                Console.WriteLine($"{key}");
+                Console.WriteLine("No PDF files found in the folder.");
             }
-
-            string fileName = Path.GetFileNameWithoutExtension(docStream.Name);
-
-            string jsonFilePath = "C:\\Users\\Usuario\\source\\repos\\PDF-Filler\\PDF-Filler\\PropsFiles\\"+ fileName + ".json";
-
-            // Save the JSON to the file
-            File.WriteAllText(jsonFilePath, json);
-
-            //Create memory stream.
-            FileStream docStream2 = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
-            //Save the document to memory stream.
-            loadedDocument.Save(docStream2);
-            //Close the document.
-            loadedDocument.Close();
         }
-
+        else
+        {
+            Console.WriteLine("The specified folder does not exist.");
+        }
     }
 }
