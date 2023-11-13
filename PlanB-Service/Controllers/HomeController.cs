c7dd96ba07cea5d031d87daf4c354a95cb48accc
Nicolás Baigorria <56775102+NicoBaigorria@users.noreply.github.com>
2023-11-13 15:58:53 -0300

    app online

---
+++ PlanB-Service/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlanB_Service.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: />
+        [HttpGet]
+        public string Get()
+        {
+            return "ok";
+        }
+    }
+}
--- PlanB-Service/Controllers/PdfFillerController.cs
+++ PlanB-Service/Controllers/PdfFillerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,18 +16,22 @@ namespace PlanB_Service.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        /*
         // GET api/<PdfFillerController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
+        */
 
         // POST api/<PdfFillerController>
         [HttpPost]
-        public string Post([FromBody] string value)
+        public string Post([FromBody] JsonObject customer)
         {
-            return "asdasd";
+            var body = new StreamReader(Request.Body);
+
+            return "asdsadas"+ customer.ToString();
         }
 
     }
