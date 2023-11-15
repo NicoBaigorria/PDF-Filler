c244a1d770172adeaec9560a1a1f0c5e54b77687
Nicolás Baigorria <56775102+NicoBaigorria@users.noreply.github.com>
2023-11-15 15:54:48 -0300

    probando iText

---
+++ PDF-Filler/FormFiller3.cs
@@ -0,0 +1,35 @@
+﻿using iText.Forms;
+using iText.Kernel.Pdf;
+using iText.Forms.Fields;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Xfa;
+
+namespace PDF_Filler
+{
+    internal class FormFiller3
+    {
+
+        public void Process(string inputFile)
+        {
+            try
+            {
+                using (PdfReader reader = new PdfReader(inputFile))
+                {
+                    PdfDocument pdfDoc = new PdfDocument(reader);
+                    PdfAcroForm acroForm = PdfAcroForm.GetAcroForm( pdfDoc, true);
+
+                    foreach (string fieldName in acroForm.GetFormFields().Keys)
+                    {
+                        Console.WriteLine("Field Name: " + fieldName);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+
+    }
+}
